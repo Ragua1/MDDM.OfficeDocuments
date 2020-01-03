75d45964824592018c233cf7e63069c53a16e6b6
Menur <david.mikulastik@mewssystems.com>
2020-01-03 17:21:13 +0100

    Constructor interfaces are represented by factories.

---
+++ OpenXmlApi/OfficeDocumentsApi.Excel/Factory/ISpreadSheetFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using OfficeDocumentsApi.Excel.Interfaces;
+using OpenXml= DocumentFormat.OpenXml;
+
+namespace OfficeDocumentsApi.Excel.Factory
+{
+    public interface ISpreadSheetFactory
+    {
+        ISpreadsheet CreateSpreadsheet(Stream stream, bool createNew);
+        ISpreadsheet CreateSpreadsheet(string filePath, bool createNew);
+
+        IWorksheet CreateWorksheet(Spreadsheet spreadsheet, OpenXml.Packaging.WorksheetPart worksheetPart, OpenXml.Spreadsheet.SheetData sheetData, IStyle cellStyle = null);
+    }
+}
