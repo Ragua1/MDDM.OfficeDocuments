66a6f410a1b5aafffe846ad8fd76cf25b4a0ea4b
Menur <david.mikulastik@mewssystems.com>
2020-01-03 17:21:45 +0100

    example factory implementations with product trainsient lifestyle.

---
+++ OpenXmlApi/OfficeDocumentsApi.Excel/Factory/Impl/RowFactory.cs
@@ -0,0 +1,18 @@
+﻿using OfficeDocumentsApi.Excel.DataClasses;
+using OfficeDocumentsApi.Excel.Interfaces;
+
+namespace OfficeDocumentsApi.Excel.Factory.Impl
+{
+    public class RowFactory : IRowFactory
+    {
+        public IRow CreateRow(IWorksheet worksheet, uint rowIndex, IStyle cellStyle = null)
+        {
+            return new Row(worksheet, rowIndex, cellStyle);
+        }
+
+        public IRow CreateRow(IWorksheet worksheet, DocumentFormat.OpenXml.Spreadsheet.Row element)
+        {
+            return new Row(worksheet, element);
+        }
+    }
+}
